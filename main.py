d0631fe9c682f9d21345cd03dd6f64fde5a6abd3
CosmicCatnap <CosmicCatnap@users.noreply.github.com>
2020-12-30 22:18:49 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/bin/python
+#input name (username not actual name)
+#<name>
+#
+username = input("Enter your name: ")
+
+#input birthday (convert to unix time)
+#xx/yy/zz
+#
+
+birthday = input("Enter your birthday in dd/mm/yy format: ")
+#randomly ask for extra input
+#
+
+#randomly ask yes or no question?
+#
+
+#fake calculating screen
+#
+
+#low quality picture
+#
+
+#present a birthday number (vision? sequence? significance?)
+#
+
+#present birthday letter
+#
+
+#present a bad nlp joke or recipe
+#
+
+#present a good constellation and a bad constellation
+#
+
+#generate a horroscope (this year do..., this year avoid)
+#
+
+#generate a music thing?
+#
+
+#present a picture
+
+print ("your name is " + username + " and your birthday is " + birthday)
