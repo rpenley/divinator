7d1a284408b170b18f0620fb4e9845789a520324
CosmicCatnap <cosmiccatnap@gmail.com>
2021-01-01 18:40:02 -0500

    I still don't get this

--- main.py
+++ main.py
@@ -5,22 +5,44 @@
 # Free to use for your pleasure...  or pain 
 ###########################################
 
-#Library imports
-import datetime, time
+# Library imports and Downloads
+import datetime, nltk, random
 from nltk.corpus import wordnet as wn
-from random import sample
-
+nltk.download('wordnet', quiet=True)
+nltk.download('gutenberg', quiet=True)
+nltk.download('punkt', quiet=True)
 #input name (username not actual name)
 #<name>
 #
-username = input("Enter your name: ")
+#username = input("Enter your name: ")
 
 # Get birthday and convert to unix time
-birthstring = input("Enter your birthday in mm/dd/yy format: ")
-birthstamp = time.mktime(datetime.datetime.strptime(birthstring, "%m/%d/%y").timetuple())
+#birthstring = input("Enter your birthday in mm/dd/yy format: ")
+#birthstamp = time.mktime(datetime.datetime.strptime(birthstring, "%m/%d/%y").timetuple())
 
 #randomly ask for extra input
 #
+#for synset in list(wn.all_synsets('n')):
+#print(sample(wn.all_synsets('n'),1))
+nouns = wn.all_synsets('n')
+print(type(nouns))
+
+bible = nltk.corpus.gutenberg.words(u'bible-kjv.txt')
+#bibleText = nltk.Text(bible)
+#bibleText.generate()
+
+# NLTK shortcuts :)
+cfd = nltk.ConditionalFreqDist(nltk.bigrams(bible))
+
+# pick a random word from the corpus to start with
+word = random.choice(bible)
+# generate 15 more words
+for i in range(15):
+    print(word, end=' ')
+    if word in cfd:
+        word = random.choice(list(cfd[word].keys()))
+    else:
+        break
 
 #randomly ask yes or no question?
 #
@@ -50,5 +72,3 @@ birthstamp = time.mktime(datetime.datetime.strptime(birthstring, "%m/%d/%y").tim
 #
 
 #present a picture
-
-print ("your name is " + username + " and your birthday is " + birthday)
